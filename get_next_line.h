a36908078e3d24a9197249101eac38bcec8f990f
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-06-30 11:35:14 +0200

    Stuff

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/30 11:26:27 by omputle           #+#    #+#             */
+/*   Updated: 2019/06/30 11:34:06 by omputle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include <unistd.h>
+# include <stdlib.c>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
