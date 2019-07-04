9f052ea621fa3f8987fb53312d1572231b9e7c7f
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-07-04 08:28:30 +0200

    Stuff

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,21 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 11:26:27 by omputle           #+#    #+#             */
-/*   Updated: 2019/06/30 11:34:06 by omputle          ###   ########.fr       */
+/*   Updated: 2019/07/03 16:50:23 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
-# include <stdlib.c>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# define BUFF_SIZE 100
+
 
 int		get_next_line(const int fd, char **line);
 
