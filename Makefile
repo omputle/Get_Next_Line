24b444d37c8f57a5c4848e4a443cec2eebf5f8d2
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-07-05 15:07:10 +0200

    GNL

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all: run
 
 run: test
-	./test
+	clear
+	./test check.txt
 
 test: main.c
 	gcc -o test main.c get_next_line.c libft/libft.a
--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,19 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 14:56:21 by omputle           #+#    #+#             */
-/*   Updated: 2019/07/04 17:24:30 by omputle          ###   ########.fr       */
+/*   Updated: 2019/07/05 15:06:02 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "get_next_line.h"
 
-static char	*ft_strappend(char *s1, char *s2)
+static char		*ft_strmerge(char *s1, char *s2)
 {
-	char	*temp;
+	char	*new;
 
-	temp = ft_strjoin(s1, s2);
+	new = ft_strjoin(s1, s2);
 	free(s1);
-	return (temp);
+	return (new);
 }
 
 static	char	*read_line(int fd, char *s)
@@ -28,46 +29,39 @@ static	char	*read_line(int fd, char *s)
 	while ((num = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[num] = '\0';
-		s = ft_strappend(s,  buff);
+		s = ft_strmerge(s, buff);
 		if (ft_strchr(s, '\n') != NULL)
 			break ;
 	}
 	return (s);
 }
 
-
-static char	*new_line(char *s, char **line)
+static char		*new_line(char *s, char **line)
 {
-	char	*temp;
+	char	*new;
 	int		count;
 
 	count = 0;
 	while (s[count] != '\n' && s[count] != '\0')
 		count++;
 	*line = ft_strsub(s, 0, count);
-//	if (ft_strcmp(*line, s) == 0)
-//		return (NULL);
-//	else
-//	{
-		temp = ft_strsub(s, count + 1, (ft_strlen(s + count + 1)));
-		free(s);
-//	}
-	return (temp);
+	new = ft_strsub(s, count + 1, (ft_strlen(s + count + 1)));
+	free(s);
+	return (new);
 }
 
-int	get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	static char	*s;
-	int	num;
-	char	buff[BUFF_SIZE + 1];
+	char		buff[BUFF_SIZE + 1];
 
 	if (!(line) || fd < 0 || read(fd, buff, 0) == -1)
 		return (-1);
-	if(!s)
+	if (!s)
 		s = ft_strnew(0);
 	if (!(ft_strchr(s, '\n')))
 		s = read_line(fd, s);
-	if (num == 0 && ft_strlen(s) == 0)
+	if (ft_strlen(s) == 0)
 		return (0);
 	s = new_line(s, line);
 	return (1);
